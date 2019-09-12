f29665ab5ab0773ca41b17d83111e4d9d39e7b77
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-12 11:40:34 +0100

    lesson 03

---
+++ lesson03-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,27 @@
+//In this lesson FreeCodeCamp want you to learn how to store values with the assignment operator
+
+
+/*
+
+FreeCodeCamp ask the following:
+
+Assign the value 7to variable a.
+
+Assign the contents of ato variable b.
+
+How it is done:
+
+*/
+
+
+// 7 is equal to a
+var a = 7;
+
+
+
+//Now assign variable b to a
+var b = a;
+
+
+//See what your value is (do not include as FreeCodeCamp solution) :
+console.log(b);
