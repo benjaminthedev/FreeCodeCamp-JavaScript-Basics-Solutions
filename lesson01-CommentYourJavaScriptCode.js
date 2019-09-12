591882ecd027650e6a56cf1ff85f2e69cdd77c65
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-12 07:53:47 +0100

    First two lessons

---
+++ lesson01-CommentYourJavaScriptCode.js
@@ -0,0 +1,12 @@
+//In this lesson FCC just want you to learn about commenting in JavaScript
+
+
+
+//This is a single line comment
+
+/*
+This is a
+Multi-line comment that you can do in JS ;)
+*/
+
+//And that is it.
