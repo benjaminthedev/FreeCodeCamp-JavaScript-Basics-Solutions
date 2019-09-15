5dbbcc31fa9135fb7fa198f3e74b1dc8f9d9bfcb
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-15 08:03:53 +0100

    updated lessons

---
+++ lesson10-Divide-One-Number-by-Another-with-JavaScript.js
@@ -0,0 +1,15 @@
+//In this lesson FreeCodeCamp want you to Divide One Number by Another with JavaScript
+
+/*
+We can also divide one number by another.
+
+JavaScript uses the /symbol for division.
+
+Example
+
+myVar = 16 / 2; // assigned 8
+
+*/
+
+
+var quotient = 66 / 33;
---
+++ lesson12-Decrement–a–Number–with-JavaScript.js
@@ -0,0 +1,24 @@
+//In this lesson FreeCodeCamp want you to Increment a Number with JavaScript
+
+/*
+You can easily decrement or decrease a variable by one with the --operator.
+
+i--;
+
+is the equivalent of
+
+i = i - 1;
+
+Note
+The entire line becomes i--;, eliminating the need for the equal sign.
+
+
+
+*/
+
+myVar --;
+
+
+//More notes here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Increment_()
+
+//Learn this well to succeed!
