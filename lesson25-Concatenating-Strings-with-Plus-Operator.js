40e00261096ce1d8ff5a906dc2256528875b6c1f
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-19 09:55:52 +0100

    added more lessons

---
+++ lesson25-Concatenating-Strings-with-Plus-Operator.js
@@ -0,0 +1,10 @@
+//In this lesson FreeCodeCamp want you to learn about Concatenating Strings with Plus Operator
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+//Don't forget the space after This is the start.
+var myStr = "This is the start. " + "This is the end.";
+
---
+++ lesson26-Concatenating-Strings-with-the-Plus-Equals-Operator.js
@@ -0,0 +1,11 @@
+//In this lesson FreeCodeCamp want you to learn about Concatenating Strings with the Plus Equals Operator
+
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
