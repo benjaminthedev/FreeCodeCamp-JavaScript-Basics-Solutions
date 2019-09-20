18def461a316cf3494c55ace611e8b477ff32507
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-20 12:00:56 +0100

    updated lessons

---
+++ lesson34-Use-Bracket-Notation-to-Find-the-Last-Character-in-a-String.js
@@ -0,0 +1,11 @@
+//In this lesson FreeCodeCamp want you to Use Bracket Notation to Find the Last Character in a String
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length -1];
+
---
+++ lesson35-Use Bracket-Notation-to-Find-the-Nth-to-Last-Character-in-a-String.js
@@ -0,0 +1,13 @@
+//In this lesson FreeCodeCamp want you to Use Bracket Notation to Use Bracket Notation to Find the Nth-to-Last Character in a String
+
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length -2];
+
