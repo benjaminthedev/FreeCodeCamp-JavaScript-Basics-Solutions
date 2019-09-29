50cc0f3017667984f6b5e03306ab7e19434bff48
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-29 09:32:47 +0100

    updated lessons

---
+++ lesson71-Logical-Order-in-If-Else-Statements.js
@@ -0,0 +1,21 @@
+//Logical Order in If Else Statements
+
+/* 
+
+Task:
+
+Change the order of logic in the function so that it will return the correct statements in all cases.
+
+*/
+function orderMyLogic(val) {
+   if (val < 5){
+        return "Less than 5";     
+    } else if (val < 10) {
+        return "Less than 10";
+    } else {
+        return "Greater than or equal to 10";
+    }
+}
+
+// Change this value to test
+orderMyLogic(7);
