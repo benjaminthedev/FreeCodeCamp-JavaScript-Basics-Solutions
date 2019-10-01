3dd7922211a7f00202bd5b330d5005b681e15717
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-10-01 11:09:09 +0100

    added more solutions

---
+++ lesson84-Updating-Object-Properties.js
@@ -0,0 +1,27 @@
+//Updating Object Properties
+
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+//Dot Notation Answer
+myDog.name = "Happy Coder";
+
+//Bracket Notation Answer - I prefer Dot Notation.
+//myDog["name"] = "Happy Coder";
